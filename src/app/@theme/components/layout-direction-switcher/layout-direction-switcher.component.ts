decee71f3b80d897aba9be55df7e09dcdee5a48a
mahmoud khalifa <mahmudkhalifa12@outlook.com>
2023-04-15 23:33:35 +0300

    added workaround to localize the side menu items

--- src/app/@theme/components/layout-direction-switcher/layout-direction-switcher.component.ts
+++ src/app/@theme/components/layout-direction-switcher/layout-direction-switcher.component.ts
@@ -5,6 +5,7 @@ import { Subject } from 'rxjs';
 import { TranslateService } from '@ngx-translate/core';
 import { LanguageTrackerService } from '../../../@core/utils/language-tracker.service';
 import { I18nService } from '../../../i18n/i18n.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ngx-layout-direction-switcher',
@@ -23,7 +24,7 @@ import { I18nService } from '../../../i18n/i18n.service';
 export class LayoutDirectionSwitcherComponent implements OnDestroy {
 
   protected destroy$ = new Subject<void>();
-  protected lang$ = new Subject<string>();
+  // protected lang$ = new Subject<string>();
 
   directions = NbLayoutDirection;
   currentDirection: NbLayoutDirection;
@@ -31,33 +32,43 @@ export class LayoutDirectionSwitcherComponent implements OnDestroy {
   @Input() vertical: boolean = false;
 
   constructor(private directionService: NbLayoutDirectionService,
-    private languageTrackerService: LanguageTrackerService,
-    private i18nService: I18nService
-    // public translate: TranslateService
+    // private languageTrackerService: LanguageTrackerService,
+    private i18nService: I18nService,
+    private router: Router,
+    public translate: TranslateService
     ) {
-    this.currentDirection = this.directionService.getDirection();
+
 
     this.directionService.onDirectionChange()
       .pipe(takeUntil(this.destroy$))
       .subscribe(newDirection => this.currentDirection = newDirection);
-  }
+
+
+      if( this.translate.currentLang === "ar-SA"){
+        this.directionService.setDirection(this.directions.RTL);
+      }
+    }
 
   toggleDirection(newDirection) {
     this.directionService.setDirection(newDirection);
 //keep this as it will change the lang
     if(newDirection == "rtl"){
       this.i18nService.language = 'ar-SA';
-
-      // this.translate.currentLang = '';
-      // this.translate.use("ar");
-      // this.languageTrackerService.SendMessage("ar");
-      // this.lang$.em
+      //([`/${url}`])
+      
+      // this.router.navigateByUrl('/',{skipLocationChange:true}).then(()=>{
+      //   this.router.navigate([`/`]).then(()=>{
+      //   console.log(`After navigation I am on:${this.router.url}`)
+      //   })
+      //   })
     }else{
       this.i18nService.language = 'en-US';
-      // this.translate.currentLang = '';
-    //  this.translate.use("en");
     //  this.languageTrackerService.SendMessage("en");
-
+    // this.router.navigateByUrl('/',{skipLocationChange:true}).then(()=>{
+    //   this.router.navigate([`/`]).then(()=>{
+    //   console.log(`After navigation I am on:${this.router.url}`)
+    //   })
+    //   })
     }
   }
 
--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,34 +1,75 @@
-import { Component, Injector, OnInit, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
+import { ChangeDetectorRef, Component, Injector, OnInit, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
 
 import { MENU_ITEMS } from './pages-menu';
 import { Router } from '@angular/router';
 import { AnalyticsService, SeoService } from '../@core/utils';
 import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 import { LanguageTrackerService } from '../@core/utils/language-tracker.service';
+import { Logger } from '../@core/utils/logger.service';
+import { NbMenuComponent } from '@nebular/theme';
 
-
+const log = new Logger('page');
 @Component({
   selector: 'ngx-pages',
   styleUrls: ['pages.component.scss'],
   template: `
     <ngx-one-column-layout>
-
-      <nb-menu [items]="menu"></nb-menu>
+    <!-- *ngIf="enableSide" -->
+      <nb-menu  #menuItems  [items]="menu"></nb-menu>
       <router-outlet></router-outlet>
     </ngx-one-column-layout>
   `,
 })
 //implements OnInit
-export class PagesComponent {
+export class PagesComponent implements OnInit {
 //here is the main page that will still appear in all the app
-  menu = MENU_ITEMS;
+  // orignalMenu = MENU_ITEMS;
+  menu = [];
+  // menu  = MENU_ITEMS.;
+
+enableSide:boolean= true;
+
+// @ViewChild('menuItems')menuItemsRef!:NbMenuComponent;
 
   constructor(
-    // public translate: TranslateService,
+    public localizationService: TranslateService,
     private languageTrackerService: LanguageTrackerService,
-    private injector: Injector,
+    private changeDetectorRef: ChangeDetectorRef,
     private router: Router)
-    {}
+    {
+        this.sideMenuTranslationInt(false);
+    }
+
+    ngOnInit(): void {
+
+      log.info(this.localizationService.currentLang);
+      this.localizationService.onLangChange.subscribe((event: LangChangeEvent) => {
+        // this.enableSide = false;
+        this.sideMenuTranslationInt(true);
+
+       });
+  }
+
+  sideMenuTranslationInt(lanChanged:boolean){
+    // let menuTranslatedas = MENU_ITEMS.slice();
+    console.log("before orignalMenu");
+     log.info(MENU_ITEMS);
+    // const menuTranslated = [...MENU_ITEMS];
+    const menuTranslated = MENU_ITEMS.map(u => ({ ...u, }));
+    menuTranslated.forEach(item => {
+      // item.title = this.localizationService.instant(item.title)
+
+      this.localizationService.get(item.title).subscribe((text:string) => {
+        // console.log(this.orignalMenu);
+        item.title = text
+      });
+
+    });
+    console.log("orignalMenu");
+     log.info(MENU_ITEMS);
+    this.menu = menuTranslated;
+   }
+
 
 }
 
